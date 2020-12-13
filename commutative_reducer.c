8cd0ff9c803f78ed03165feb4bb802e1e725b6de
Qi Qi <qqi@mit.edu>
2020-12-13 22:13:46 +0000

    Use aligned heap allocation for commutative reducer

--- commutative_reducer.c
+++ commutative_reducer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
 #include "fasttime.h"
@@ -7,13 +8,20 @@
 
 #define CILK_NWORKERS 16
 
-
+// Without "Atomic", reading directly from tls_worker can cause a wrong answer
+//extern __thread _Atomic(struct __cilkrts_worker *)tls_worker;
 extern __thread struct __cilkrts_worker *tls_worker;
 
 
 void eval_commutative_red(Vector* arr, int* indices) {
-  Vector local_views[CILK_NWORKERS] = {};
-  Vector n = {};
+  Vector* local_views;
+  //local_views = malloc(CILK_NWORKERS * sizeof(Vector));
+  local_views = aligned_alloc(64 * 8, CILK_NWORKERS * sizeof(Vector));
+  memset(local_views, 0, CILK_NWORKERS * sizeof(Vector));
+
+  Vector* n;
+  n = malloc(sizeof(Vector));
+  memset(n, 0, sizeof(Vector));
 
   fasttime_t start = gettime();
   // Sum
@@ -23,13 +31,16 @@ void eval_commutative_red(Vector* arr, int* indices) {
     vector_add(&local_views[worker_number], &arr[indices[i]]);
   }
   for (int i = 0; i < CILK_NWORKERS; i++) {
-    vector_add(&n, &local_views[i]);
+    vector_add(n, &local_views[i]);
   }
   fasttime_t stop = gettime();
 
   long sum = 0;
   for (int i = 0; i < VECTOR_LEN; i++) {
-    sum += n.ele[i];
+    sum += n->ele[i];
   }
   printf("%f\t%ld\n", tdiff_sec(start, stop), sum);
+
+  free(local_views);
+  free(n);
 }
